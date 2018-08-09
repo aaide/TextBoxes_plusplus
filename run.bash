8b4fc9a19ee435b49d9c8a2369d82c469a0ff97d
tobias.hermann <tobias.hermann@unternehmertum.de>
2018-08-09 17:49:48 +0200

    Minor refactor of examples/text/demo.py

--- run.bash
+++ run.bash
@@ -9,4 +9,5 @@ nvidia-docker run -it \
                   --rm \
                   -v ${PWD}/models:/opt/caffe/models \
                   -v ${PWD}/crnn/model:/opt/caffe/crnn/model \
+                  -v ${PWD}/demo_images:/opt/caffe/demo_images \
                   tbpp_crnn:gpu bash
